c405616f8f01c18f8aee52840d007b2e6da108ac
ScytaleTleilax <andreivradulescu@gmail.com>
2016-09-08 00:54:47 +0300

    created modular routes

---
+++ routes/contactsID.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.router();
+
+//find a single contact by ID
+router.get("/contacts/:id",function(req,res){
+
+});
+
+//Update entire contact document
+router.put("/contacts/:id",function(req,res){
+
+});
+
+//Delete a contact by ID
+router.delete("/contacts/:id",function(req,res){
+
+});
