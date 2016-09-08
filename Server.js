09be49222a4c6de28d1245f037ccde8fbfed9fcd
ScytaleTleilax <andreivradulescu@gmail.com>
2016-09-08 19:32:18 +0300

    Refactoring

--- Server.js
+++ Server.js
@@ -14,39 +14,35 @@ app.use(bodyParser.json());
 var db;
 
 process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/';
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-});
+mongodb.MongoClient.connect(
+	process.env.MONGODB_URI, function (err, database)
+	{
+		if (err)
+		{
+			console.log(err);
+			process.exit(1);
+		}
+	});
 
 // Save database object from the callback for reuse.
 db = database;
 console.log("Database connection ready");
 
-var server = app.listen(process.env.PORT || 8080, function () {
-    console.log("app now running on port ${server.address().port}")
-});
-
+var server = app.listen(
+	process.env.PORT || 8080, function ()
+	{
+		console.log("app now running on port ${server.address().port}")
+	});
 //generic error handler
-
-function handleError(res,reason,message,code){
-    console.log("ERROR:"+reason);
-    res.status(code || 500).json({"error":message,"reason":reason});
+function handleError(res, reason, message, code)
+{
+	console.log("ERROR:" + reason);
+	res.status(code || 500)
+	   .json({"error": message, "reason": reason});
 }
 
 //http://stackoverflow.com/questions/28305120/differences-between-express-router-and-app-get
-app.use('/contacts',require('./routes/contacts'));
-app.use('/contacts/:id',require('./routes/contactsID'));
-
-
-
-
-
-
-
-
+app.use('/contacts/*', require('./routes/contacts'));
 
 
 
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,55 @@
 const express = require("express");
 const router = express.router();
+var app = require('../Server.js');
 
 
+router.route("/contacts")
+      .all(function (req, res, next)
+           {
+	           console.log("main Middleware");
+	           next();
+           })//find all contacts
+      .get(function (req, res, next)
+           {
 
-//find all contacts
-router.get("/contacts",function(req,res){
-    
-});
+	           next();
+           })
+      .post(function (req, res)
+            {
+	            var newContact = req.body;
+	            newContact.createDate = new Date();
+
+	            if (!(req.body.firstName || req.body.lastName))
+	            {
+		            app.handleError(res, "Invalid input", "Must provide first or last name", 400)
+	            }
+            });
 
 
-//Create a new contact
-app.post("/contacts",function(req,res){
+router.route("/contacts/:id")
+      .all(
+	      function (req, res, next)
+	      {
+		      var time = new Date().getDate()
+		                           .toString();
+		      console.log(time);
+		      next();
+	      })//find a single contact by ID
+      .get(
+	      function (req, res, next)
+	      {
 
-});
+		      next();
+	      })//Update entire contact document
+      .put(
+	      function (req, res, next)
+	      {
+
+		      next();
+	      })//Delete a contact by ID
+      .delete(
+	      function (req, res, next)
+	      {
+
+		      next();
+	      });
